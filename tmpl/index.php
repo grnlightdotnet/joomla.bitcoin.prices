587fafb58ac07c574f1ca42e10d372a1f581c21e
grnlightdotnet <admin@grnlight.net>
2013-11-22 00:26:54 -0500

    Create index.php

---
+++ tmpl/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Helper class for Bitcoin Pricing Joomla Module
+ * 
+ * @package    Joomla
+ * @link 	http://www.grnlight.net/index.php/bitcoin-prices-module
+ * @license        GPL v3, see LICENSE.php
+ * mod_helloworld is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+ 
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
